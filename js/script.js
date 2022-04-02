1d79ea73f922e6050376ac63247e76545604e909
ViRack <vincentm_2@hotmail.com>
2022-04-01 23:27:25 -0700

    skeleton for script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const btn = document.quertSelector('button');
-let value1, value2;
+let expression = "";
 
-btn.addEventListener('click', readValue());
-
-
-function readValue() {
-    
+const btn = document.querySelectorAll("button");
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function() {
+        console.log("in readValue");
+        let thisChar = btn[i].textContent;
+        expression += thisChar;
+        console.log(expression);
+    });
 }
